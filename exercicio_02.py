e129cdece93044e247a52ba51ce59a991b921cbf
Guilhermesantosrgb <Megacofre86@gmail.com>
2025-09-03 11:04:58 -0300

    Add files via upload

---
+++ exercicio_02.py
@@ -0,0 +1,10 @@
+dobro: int
+triplo: int
+raiz: float
+numero: int
+numero= input(('Digite um numero e eu farei ele no dobro, triplo e a raiz dele:'))
+dobro= numero * 2
+triplo= numero * 3
+
+
+print(f'dobro:{dobro}, triplo:{triplo},')
---
+++ exercicio_03.py
@@ -0,0 +1,8 @@
+
+metros= int(input('Digite um numero em metros e eu irei converte-lo para centimetros e milimetros'))
+
+centimetros= metros * 100
+milimetros= metros * 1000
+
+print(f'seus metros em centimetros:',centimetros)
+print(f' agora em milimetros:',milimetros)
